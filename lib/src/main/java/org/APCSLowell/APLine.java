5b283bdc4571a860cb280e80fce28684cf4d348f
willthepan <williampan.us@gmail.com>
2026-04-24 03:47:16 +0000

    final

--- lib/src/main/java/org/APCSLowell/APLine.java
+++ lib/src/main/java/org/APCSLowell/APLine.java
@@ -2,7 +2,13 @@ package org.APCSLowell;
 
 public class APLine {
     int a, b, c;
-    public APLine(int a, int b, int c) {}
+    public APLine(int a, int b, int c) {
+        this.a=a;
+        this.b=b;
+        this.c=c;
+
+
+    }
     public boolean isOnLine(int x, int y) {
         return a * x + b * y + c == 0;
     }
